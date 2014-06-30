89707531ca0318ad89e7fcdd48a6923618da331d
rahul@codebase.bz <rahul@codebase.bz>
2014-06-30 16:11:35 +0530

    comments added

--- src/com/example/sqltester/MainActivity.java
+++ src/com/example/sqltester/MainActivity.java
@@ -15,21 +15,24 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
     
+	// declaring ListView
 	ListView listView;
-
+    
+	// initializing an ArrayList of Strings to couple with Adapter
 	ArrayList<String> b = new ArrayList<String>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
         
+	    // adding items to populate in ListView 
 		b.add("Liquorice");
 		b.add("Dairy Milk");
 		b.add("Rice Crispies");
 		b.add("Orange-Mint");
 		b.add("After-Tens");
 		
-		
+		// Initializing and setting adapter for ListView
 		listView = (ListView) findViewById(R.id.list_view);
 		ArrayAdapter adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.layout_file,b);
 	    listView.setAdapter(adapter);
@@ -48,7 +51,9 @@ public class MainActivity extends Activity {
 		 EditText editText = (EditText) findViewById(R.id.edit_text);
 		 String data = editText.getText().toString();
 		 // manipulating the data source coupled to the adapter will change the ListView
-		 // in addition to adapter.add((String)data). 
+		 // in addition to adapter.add((String)data) and this can cause duplications or
+		 // unexpected results to occur with the ListView. Commented out for the sake of clarity
+		 // and also to avoid duplications. 
 		 // b.add(data);
 		 ArrayAdapter adapter = (ArrayAdapter) listView.getAdapter();
 		 adapter.add((String)data);
